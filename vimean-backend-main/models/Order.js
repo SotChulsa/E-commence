6e0b841ee41373e8889bd417e79546db07ddb28f
Your Name <you@example.com>
2026-03-18 13:05:09 +0700

    removed backend

--- vimean-backend-main/models/Order.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-
-const orderSchema = new mongoose.Schema({
-  user: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
-  items: Array,
-  totalPrice: Number,
-});
-
-module.exports = mongoose.model("Order", orderSchema);
--- vimean-backend-main/routes/orderRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { createOrder } = require("../controller/orderController");
-const { protect } = require("../middleware/authMiddleware");
-
-router.post("/", protect, createOrder);
-
-module.exports = router;
